34894960a80c4892a0cb229201ec0a5d1bb745e8
8374654344 <rameshgongura@gmail.com>
2024-02-15 20:01:04 +0530

    file updated

---
+++ AddingNumbersUsingFunctionalInterfaces.java
@@ -0,0 +1,13 @@
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+
+interface Adder {
+    int add(int a, int b);
+}
+public class AddingNumbersUsingFunctionalInterfaces {
+    public static void main(String[] args){
+        Adder adder = (a,b)-> a + b;
+         int result = adder.add(2,3);
+        System.out.println(result);
+    }
+}
